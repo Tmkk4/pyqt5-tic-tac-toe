598ef9928aeabedac42cc7513df7318b8b571e87
TKWA-PADX1E-LPT\Oreo <okakm.k.h30@gmail.com>
2022-05-18 11:19:59 +0900

    PyQtによる三目並べ main window : QGraphicsView <- Qwidget

---
+++ study/qgraphics_tic-tac-toe-v_1_0.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+"""
+    Tic Tac Toe on PyQt5 Python3.10(conda)
+    System Development A 2022/05/17
+
+    quote :
+    Window : https://www.sejuku.net/blog/75467
+    Widgets,Graphics :
+"""
+
+import sys
+from PyQt5.QtCore import QLineF, QPointF, QRectF, Qt
+from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsItem, QGridLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton
+from PyQt5.QtGui import QBrush, QColor, QPainter
